e3cb941e8b703898c7faf88d19328442e9937495
wenjiu <2784307979@qq.com>
2025-03-31 05:48:56 +0800

    init: nzea impl for simulator
    Signed-off-by: wenjiu <2784307979@qq.com>

--- src/simulator.rs
+++ src/simulator.rs
@@ -1,15 +1,37 @@
-// use option_parser::OptionParser;
+use option_parser::OptionParser;
+use remu_utils::ProcessResult;
 use state::States;
+
+use crate::{SimulatorCallback, SimulatorError, SimulatorItem};
 
 use super::Top;
 
 pub struct Nzea {
     pub top: Top,
     pub states: States,
+    pub callback: SimulatorCallback,
 }
 
 impl Nzea {
-    // fn new(option: &OptionParser, states: States, callback: SimulatorCallback) -> Self {
+    pub fn new(_option: &OptionParser, states: States, callback: SimulatorCallback) -> Self {
+        let top = Top::new();
 
-    // }
+        Self {
+            top,
+            states,
+            callback,
+        }
+    }
+}
+
+impl SimulatorItem for Nzea {
+    fn init(&self) -> Result<(), SimulatorError> {
+        self.top.reset(100);
+        Ok(())
+    }
+
+    fn step_cycle(&mut self) -> ProcessResult<()> {
+        self.top.cycle(1);
+        Ok(())
+    }
 }
