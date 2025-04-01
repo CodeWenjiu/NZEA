dd93bf7557b35e3aa2943f8a980655099571f5c9
wenjiu <2784307979@qq.com>
2025-04-01 08:08:21 +0800

    chore(wip): change work place
    Signed-off-by: wenjiu <2784307979@qq.com>

--- src/simulator.rs
+++ src/simulator.rs
@@ -1,24 +1,148 @@
+use std::sync::OnceLock;
+
 use option_parser::OptionParser;
+use owo_colors::OwoColorize;
+use remu_macro::log_debug;
+use logger::Logger;
 use remu_utils::ProcessResult;
 use state::States;
 
 use crate::{SimulatorCallback, SimulatorError, SimulatorItem};
 
-use super::Top;
+use super::{BasicCallbacks, Input, NpcCallbacks, Output, Top};
+
+pub struct NzeaTimes {
+    pub cycles: u64,
+    pub instructions: u64,
+
+    pub alu_catch: u64,
+    pub idu_catch: u64,
+    pub ifu_catch: u64,
+
+    pub icache_map_miss: u64,
+    pub icache_cache_miss: u64,
+    pub icache_cache_hit: u64,
+}
+
+static mut NZEA_TIMES: NzeaTimes = NzeaTimes {
+    cycles: 0,
+    instructions: 0,
+
+    alu_catch: 0,
+    idu_catch: 0,
+    ifu_catch: 0,
+
+    icache_map_miss: 0,
+    icache_cache_miss: 0,
+    icache_cache_hit: 0,
+};
+
+thread_local! {
+    static NZEA_MMU: OnceLock<States> = OnceLock::new();
+}
+
+unsafe extern "C" fn alu_catch_handler() {
+    unsafe { NZEA_TIMES.alu_catch += 1 };
+}
+
+unsafe extern "C" fn idu_catch_handler(_inst_type: Input) {
+    log_debug!("idu_catch_p");
+}
+
+unsafe extern "C" fn ifu_catch_handler(_pc: Input, _inst: Input) {
+    log_debug!("ifu_catch_p");
+}
+
+unsafe extern "C" fn icache_mat_catch_handler(_count: Input) {
+    log_debug!("icache_mat_catch_p");
+}
+
+unsafe extern "C" fn icache_catch_handler(_map_hit: u8, _cache_hit: u8) {
+    log_debug!("icache_catch_p");
+}
+
+unsafe extern "C" fn icache_flush_handler() {
+    log_debug!("icache_flush_p");
+}
+
+unsafe extern "C" fn icache_state_catch_handler(
+    _write_index: Input,
+    _write_way: Input,
+    _write_tag: Input,
+    _write_data: Input,
+) {
+    log_debug!("icache_state_catch_p");
+}
+
+unsafe extern "C" fn lsu_catch_handler(_diff_skip: u8) {
+    log_debug!("lsu_catch_p");
+}
+
+unsafe extern "C" fn pipeline_catch_handler() {
+    log_debug!("pipeline_catch_p");
+}
+
+unsafe extern "C" fn uart_catch_handler(_c: Input) {
+    log_debug!("uart_catch");
+}
+
+unsafe extern "C" fn wbu_catch_handler(
+    _next_pc: Input,
+    _gpr_waddr: Input,
+    _gpr_wdata: Input,
+    _csr_wena: Input,
+    _csr_waddra: Input,
+    _csr_wdataa: Input,
+    _csr_wenb: Input,
+    _csr_waddrb: Input,
+    _csr_wdatab: Input,
+) {
+    log_debug!("wbu_catch");
+}
+
+unsafe extern "C" fn sram_read_handler(_addr: Input, _data: Output) {
+    log_debug!("sram_read");
+}
+
+unsafe extern "C" fn sram_write_handler(_addr: Input, _data: Input, _mask: Input) {
+    log_debug!("sram_write");
+}
 
 pub struct Nzea {
     pub top: Top,
-    pub states: States,
     pub callback: SimulatorCallback,
 }
 
 impl Nzea {
     pub fn new(_option: &OptionParser, states: States, callback: SimulatorCallback) -> Self {
         let top = Top::new();
+        NZEA_MMU.with(|mmu| {
+            let _ = mmu.set(states);
+        });
+
+        let basic_callbacks = BasicCallbacks {
+            alu_catch_p: alu_catch_handler,
+            idu_catch_p: idu_catch_handler,
+            ifu_catch_p: ifu_catch_handler,
+            icache_mat_catch_p: icache_mat_catch_handler,
+            icache_catch_p: icache_catch_handler,
+            icache_flush_p: icache_flush_handler,
+            icache_state_catch_p: icache_state_catch_handler,
+            lsu_catch_p: lsu_catch_handler,
+            pipeline_catch_p: pipeline_catch_handler,
+            wbu_catch: wbu_catch_handler,
+        };
+        
+        let npc_callbacks = NpcCallbacks {
+            uart_catch: uart_catch_handler,
+            sram_read: sram_read_handler,
+            sram_write: sram_write_handler,
+        };
+
+        top.init(basic_callbacks, npc_callbacks);
 
         Self {
             top,
-            states,
             callback,
         }
     }
@@ -32,6 +156,19 @@ impl SimulatorItem for Nzea {
 
     fn step_cycle(&mut self) -> ProcessResult<()> {
         self.top.cycle(1);
+        
+        Ok(())
+    }
+
+    fn times(&self) -> ProcessResult<()> {
+        println!("{}: {}", "Cycles\t".purple(), NZEA_TIMES.cycles.blue());
+        println!("{}: {}", "Instructions\t".purple(), NZEA_TIMES.instructions.blue());
+        println!("{}: {}", "ALU\t".purple(), NZEA_TIMES.alu_catch.blue());
+        println!("{}: {}", "IDU\t".purple(), NZEA_TIMES.idu_catch.blue());
+        println!("{}: {}", "IFU\t".purple(), NZEA_TIMES.ifu_catch.blue());
+
+        println!("{}: {}", "ICache Hit rate[In region]\t".purple(), (NZEA_TIMES.icache_cache_hit as f64 / (NZEA_TIMES.icache_cache_hit + NZEA_TIMES.icache_cache_miss) as f64).blue());
+        println!("{}: {}", "ICache Miss rate[Out region]\t".purple(), (NZEA_TIMES.icache_cache_hit as f64 / (NZEA_TIMES.icache_cache_hit + NZEA_TIMES.icache_cache_miss + NZEA_TIMES.icache_map_miss) as f64).blue());
         Ok(())
     }
 }
--- wrapper/wrapper.cpp
+++ wrapper/wrapper.cpp
@@ -2,76 +2,67 @@
 #include <iostream>
 
 extern "C" {
-    void ALU_catch() {
-        std::cout << "ALU_catch called" << std::endl;
-        basic_callbacks.ALU_catch_p();
-    }
-
-    void IDU_catch(const svBitVecVal *Inst_Type) {
-        std::cout << "IDU_catch called" << std::endl;
-        basic_callbacks.IDU_catch_p(Inst_Type);
-    }
-
-    void IFU_catch(const svBitVecVal *pc, const svBitVecVal *inst) {
-        std::cout << "IFU_catch called" << std::endl;
-        basic_callbacks.IFU_catch_p(pc, inst);
-    }
-
-    void Icache_MAT_catch(const svBitVecVal *count) {
-        std::cout << "Icache_MAT_catch called" << std::endl;
-        basic_callbacks.Icache_MAT_catch_p(count);
-    }
-
-    void Icache_catch(svBit map_hit, svBit cache_hit) {
-        std::cout << "Icache_catch called" << std::endl;
-        basic_callbacks.Icache_catch_p(map_hit, cache_hit);
-    }
-
-    void Icache_flush() {
-        std::cout << "Icache_flush called" << std::endl;
-        basic_callbacks.Icache_flush_p();
-    }
-
-    void Icache_state_catch(const svBitVecVal *write_index, const svBitVecVal *write_way, const svBitVecVal *write_tag, const svBitVecVal *write_data) {
-        std::cout << "Icache_state_catch called" << std::endl;
-        basic_callbacks.Icache_state_catch_p(write_index, write_way, write_tag, write_data);
-    }
-
-    void LSU_catch(svBit diff_skip) {
-        std::cout << "LSU_catch called" << std::endl;
-        basic_callbacks.LSU_catch_p(diff_skip);
-    }
-
-    void Pipeline_catch() {
-        std::cout << "Pipeline_catch called" << std::endl;
-        basic_callbacks.Pipeline_catch_p();
-    }
-
-    void Uart_putc(const svBitVecVal *c) {
-        std::cout << "Uart_putc called" << std::endl;
-        npc_callbacks.Uart_putc_p(c);
-    }
-
-    void WBU_catch(const svBitVecVal *next_pc, const svBitVecVal *gpr_waddr, const svBitVecVal *gpr_wdata, const svBitVecVal *csr_wena, const svBitVecVal *csr_waddra, const svBitVecVal *csr_wdataa, const svBitVecVal *csr_wenb, const svBitVecVal *csr_waddrb, const svBitVecVal *csr_wdatab) {
-        std::cout << "WBU_catch called" << std::endl;
-        basic_callbacks.WBU_catch_p(next_pc, gpr_waddr, gpr_wdata, csr_wena, csr_waddra, csr_wdataa, csr_wenb, csr_waddrb, csr_wdatab);
-    }
-
-    void sram_read(const svBitVecVal *addr, svBitVecVal *data) {
-        std::cout << "sram_read called" << std::endl;
-        npc_callbacks.sram_read_p(addr, data);
-    }
-
-    void sram_write(const svBitVecVal *addr, const svBitVecVal *data, const svBitVecVal *mask) {
-        std::cout << "sram_write called" << std::endl;
-        npc_callbacks.sram_write_p(addr, data, mask);
-    }
+    // basic callbacks
 
     void set_basic_callbacks(Basic_Callbacks cb) {
         basic_callbacks = cb;
     }
 
+    void ALU_catch() {
+        basic_callbacks.ALU_catch_p();
+    }
+
+    void IDU_catch(const svBitVecVal *Inst_Type) {
+        basic_callbacks.IDU_catch_p(Inst_Type);
+    }
+
+    void IFU_catch(const svBitVecVal *pc, const svBitVecVal *inst) {
+        basic_callbacks.IFU_catch_p(pc, inst);
+    }
+
+    void Icache_MAT_catch(const svBitVecVal *count) {
+        basic_callbacks.Icache_MAT_catch_p(count);
+    }
+
+    void Icache_catch(svBit map_hit, svBit cache_hit) {
+        basic_callbacks.Icache_catch_p(map_hit, cache_hit);
+    }
+
+    void Icache_flush() {
+        basic_callbacks.Icache_flush_p();
+    }
+
+    void Icache_state_catch(const svBitVecVal *write_index, const svBitVecVal *write_way, const svBitVecVal *write_tag, const svBitVecVal *write_data) {
+        basic_callbacks.Icache_state_catch_p(write_index, write_way, write_tag, write_data);
+    }
+
+    void LSU_catch(svBit diff_skip) {
+        basic_callbacks.LSU_catch_p(diff_skip);
+    }
+
+    void Pipeline_catch() {
+        basic_callbacks.Pipeline_catch_p();
+    }
+
+    void WBU_catch(const svBitVecVal *next_pc, const svBitVecVal *gpr_waddr, const svBitVecVal *gpr_wdata, const svBitVecVal *csr_wena, const svBitVecVal *csr_waddra, const svBitVecVal *csr_wdataa, const svBitVecVal *csr_wenb, const svBitVecVal *csr_waddrb, const svBitVecVal *csr_wdatab) {
+        basic_callbacks.WBU_catch_p(next_pc, gpr_waddr, gpr_wdata, csr_wena, csr_waddra, csr_wdataa, csr_wenb, csr_waddrb, csr_wdatab);
+    }
+
+    // npc callbacks
+
     void set_npc_callbacks(NPC_Callbacks cb) {
         npc_callbacks = cb;
+    }
+
+    void Uart_putc(const svBitVecVal *c) {
+        npc_callbacks.Uart_putc_p(c);
+    }
+
+    void sram_read(const svBitVecVal *addr, svBitVecVal *data) {
+        npc_callbacks.sram_read_p(addr, data);
+    }
+
+    void sram_write(const svBitVecVal *addr, const svBitVecVal *data, const svBitVecVal *mask) {
+        npc_callbacks.sram_write_p(addr, data, mask);
     }
 }
