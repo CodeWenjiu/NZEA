a9b83e9936f5946e29399bfd3ceca876e4c74ee3
wenjiu <2784307979@qq.com>
2025-04-10 05:26:53 +0800

    feat: add NZ-jyd platform
    Signed-off-by: wenjiu <2784307979@qq.com>

--- wrapper/wrapper.cpp
+++ wrapper/wrapper.cpp
@@ -77,6 +77,9 @@ class VTop_container: public Vtop {
                 delete tfp;
                 tfp = nullptr;
             }
+            if (nvboard_on) {
+                nvboard_quit();
+            }
             this->final();
         }
 };
