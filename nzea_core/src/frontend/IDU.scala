180b6329ed2b71aa4ff9f041570915ef01ba9912
wenjiu <2784307979@qq.com>
2026-02-22 11:08:11 +0800

    wip: refactor

--- nzea_core/src/frontend/IDU.scala
+++ nzea_core/src/frontend/IDU.scala
@@ -3,12 +3,22 @@ package nzea_core.frontend
 import chisel3._
 import chisel3.util.BitPat
 import chisel3.util.Decoupled
+import chisel3.util.{Cat, Fill, Mux1H}
 import chisel3.util.experimental.decode.{DecodeField, DecodePattern, TruthTable, QMCMinimizer, decoder}
 
 // -------- DecodePattern: instruction patterns (32-bit) --------
 
-/** RISC-V instruction pattern: machine code (bitPat) + metadata for decode. */
-case class RVInst(name: String, bitPatStr: String, instType: String) extends DecodePattern {
+/** One-hot encoding for Mux1H; better timing than binary. */
+object ImmType extends chisel3.ChiselEnum {
+  val I = Value((1 << 0).U)
+  val S = Value((1 << 1).U)
+  val B = Value((1 << 2).U)
+  val U = Value((1 << 3).U)
+  val J = Value((1 << 4).U)
+}
+
+/** RISC-V instruction pattern: machine code (bitPat) + imm type for decode. None = dontCare for QMC. */
+case class RVInst(name: String, bitPatStr: String, immType: Option[ImmType.Type]) extends DecodePattern {
   def bitPat: BitPat = BitPat(bitPatStr)
 }
 
@@ -17,63 +27,63 @@ object RiscvInsts {
   def n(n: Int) = "?" * n
 
   // U-type: LUI=0110111, AUIPC=0010111
-  val LUI   = RVInst("LUI",   "b" + n(25) + "0110111", "U")
-  val AUIPC = RVInst("AUIPC", "b" + n(25) + "0010111", "U")
+  val LUI   = RVInst("LUI",   "b" + n(25) + "0110111", Some(ImmType.U))
+  val AUIPC = RVInst("AUIPC", "b" + n(25) + "0010111", Some(ImmType.U))
 
   // J-type: JAL=1101111
-  val JAL   = RVInst("JAL",   "b" + n(25) + "1101111", "J")
+  val JAL   = RVInst("JAL",   "b" + n(25) + "1101111", Some(ImmType.J))
 
   // I-type (JALR=1100111, load=0000011, op-imm=0010011, system=1110011)
-  val JALR  = RVInst("JALR",  "b" + n(12) + n(5) + "000" + n(5) + "1100111", "I")
-  val LB    = RVInst("LB",    "b" + n(7) + n(5) + n(5) + "000" + n(5) + "0000011", "I")
-  val LH    = RVInst("LH",    "b" + n(7) + n(5) + n(5) + "001" + n(5) + "0000011", "I")
-  val LW    = RVInst("LW",    "b" + n(7) + n(5) + n(5) + "010" + n(5) + "0000011", "I")
-  val LBU   = RVInst("LBU",   "b" + n(7) + n(5) + n(5) + "100" + n(5) + "0000011", "I")
-  val LHU   = RVInst("LHU",   "b" + n(7) + n(5) + n(5) + "101" + n(5) + "0000011", "I")
-  val ADDI  = RVInst("ADDI",  "b" + n(7) + n(5) + n(5) + "000" + n(5) + "0010011", "I")
-  val SLTI  = RVInst("SLTI",  "b" + n(7) + n(5) + n(5) + "010" + n(5) + "0010011", "I")
-  val SLTIU = RVInst("SLTIU", "b" + n(7) + n(5) + n(5) + "011" + n(5) + "0010011", "I")
-  val XORI  = RVInst("XORI",  "b" + n(7) + n(5) + n(5) + "100" + n(5) + "0010011", "I")
-  val ORI   = RVInst("ORI",   "b" + n(7) + n(5) + n(5) + "110" + n(5) + "0010011", "I")
-  val ANDI  = RVInst("ANDI",  "b" + n(7) + n(5) + n(5) + "111" + n(5) + "0010011", "I")
-  val SLLI  = RVInst("SLLI",  "b0000000" + n(5) + n(5) + "001" + n(5) + "0010011", "I")
-  val SRLI  = RVInst("SRLI",  "b0000000" + n(5) + n(5) + "101" + n(5) + "0010011", "I")
-  val SRAI  = RVInst("SRAI",  "b0100000" + n(5) + n(5) + "101" + n(5) + "0010011", "I")
-  val ECALL = RVInst("ECALL", "b00000000000000000000000001110011", "I")
-  val EBREAK= RVInst("EBREAK","b00000000000100000000000001110011", "I")
-  val CSRRW  = RVInst("CSRRW",  "b" + n(12) + n(5) + "001" + n(5) + "1110011", "I")
-  val CSRRS  = RVInst("CSRRS",  "b" + n(12) + n(5) + "010" + n(5) + "1110011", "I")
-  val CSRRC  = RVInst("CSRRC",  "b" + n(12) + n(5) + "011" + n(5) + "1110011", "I")
-  val CSRRWI = RVInst("CSRRWI", "b" + n(12) + n(5) + "101" + n(5) + "1110011", "I")
-  val CSRRSI = RVInst("CSRRSI", "b" + n(12) + n(5) + "110" + n(5) + "1110011", "I")
-  val CSRRCI = RVInst("CSRRCI", "b" + n(12) + n(5) + "111" + n(5) + "1110011", "I")
+  val JALR  = RVInst("JALR",  "b" + n(12) + n(5) + "000" + n(5) + "1100111", Some(ImmType.I))
+  val LB    = RVInst("LB",    "b" + n(7) + n(5) + n(5) + "000" + n(5) + "0000011", Some(ImmType.I))
+  val LH    = RVInst("LH",    "b" + n(7) + n(5) + n(5) + "001" + n(5) + "0000011", Some(ImmType.I))
+  val LW    = RVInst("LW",    "b" + n(7) + n(5) + n(5) + "010" + n(5) + "0000011", Some(ImmType.I))
+  val LBU   = RVInst("LBU",   "b" + n(7) + n(5) + n(5) + "100" + n(5) + "0000011", Some(ImmType.I))
+  val LHU   = RVInst("LHU",   "b" + n(7) + n(5) + n(5) + "101" + n(5) + "0000011", Some(ImmType.I))
+  val ADDI  = RVInst("ADDI",  "b" + n(7) + n(5) + n(5) + "000" + n(5) + "0010011", Some(ImmType.I))
+  val SLTI  = RVInst("SLTI",  "b" + n(7) + n(5) + n(5) + "010" + n(5) + "0010011", Some(ImmType.I))
+  val SLTIU = RVInst("SLTIU", "b" + n(7) + n(5) + n(5) + "011" + n(5) + "0010011", Some(ImmType.I))
+  val XORI  = RVInst("XORI",  "b" + n(7) + n(5) + n(5) + "100" + n(5) + "0010011", Some(ImmType.I))
+  val ORI   = RVInst("ORI",   "b" + n(7) + n(5) + n(5) + "110" + n(5) + "0010011", Some(ImmType.I))
+  val ANDI  = RVInst("ANDI",  "b" + n(7) + n(5) + n(5) + "111" + n(5) + "0010011", Some(ImmType.I))
+  val SLLI  = RVInst("SLLI",  "b0000000" + n(5) + n(5) + "001" + n(5) + "0010011", Some(ImmType.I))
+  val SRLI  = RVInst("SRLI",  "b0000000" + n(5) + n(5) + "101" + n(5) + "0010011", Some(ImmType.I))
+  val SRAI  = RVInst("SRAI",  "b0100000" + n(5) + n(5) + "101" + n(5) + "0010011", Some(ImmType.I))
+  val ECALL = RVInst("ECALL", "b00000000000000000000000001110011", Some(ImmType.I))
+  val EBREAK= RVInst("EBREAK","b00000000000100000000000001110011", Some(ImmType.I))
+  val CSRRW  = RVInst("CSRRW",  "b" + n(12) + n(5) + "001" + n(5) + "1110011", Some(ImmType.I))
+  val CSRRS  = RVInst("CSRRS",  "b" + n(12) + n(5) + "010" + n(5) + "1110011", Some(ImmType.I))
+  val CSRRC  = RVInst("CSRRC",  "b" + n(12) + n(5) + "011" + n(5) + "1110011", Some(ImmType.I))
+  val CSRRWI = RVInst("CSRRWI", "b" + n(12) + n(5) + "101" + n(5) + "1110011", Some(ImmType.I))
+  val CSRRSI = RVInst("CSRRSI", "b" + n(12) + n(5) + "110" + n(5) + "1110011", Some(ImmType.I))
+  val CSRRCI = RVInst("CSRRCI", "b" + n(12) + n(5) + "111" + n(5) + "1110011", Some(ImmType.I))
 
   // S-type: store=0100011
-  val SB = RVInst("SB", "b" + n(7) + n(5) + n(5) + "000" + n(5) + "0100011", "S")
-  val SH = RVInst("SH", "b" + n(7) + n(5) + n(5) + "001" + n(5) + "0100011", "S")
-  val SW = RVInst("SW", "b" + n(7) + n(5) + n(5) + "010" + n(5) + "0100011", "S")
+  val SB = RVInst("SB", "b" + n(7) + n(5) + n(5) + "000" + n(5) + "0100011", Some(ImmType.S))
+  val SH = RVInst("SH", "b" + n(7) + n(5) + n(5) + "001" + n(5) + "0100011", Some(ImmType.S))
+  val SW = RVInst("SW", "b" + n(7) + n(5) + n(5) + "010" + n(5) + "0100011", Some(ImmType.S))
 
   // B-type: branch=1100011
-  val BEQ  = RVInst("BEQ",  "b" + n(7) + n(5) + n(5) + "000" + n(5) + "1100011", "B")
-  val BNE  = RVInst("BNE",  "b" + n(7) + n(5) + n(5) + "001" + n(5) + "1100011", "B")
-  val BLT  = RVInst("BLT",  "b" + n(7) + n(5) + n(5) + "100" + n(5) + "1100011", "B")
-  val BGE  = RVInst("BGE",  "b" + n(7) + n(5) + n(5) + "101" + n(5) + "1100011", "B")
-  val BLTU = RVInst("BLTU", "b" + n(7) + n(5) + n(5) + "110" + n(5) + "1100011", "B")
-  val BGEU = RVInst("BGEU", "b" + n(7) + n(5) + n(5) + "111" + n(5) + "1100011", "B")
+  val BEQ  = RVInst("BEQ",  "b" + n(7) + n(5) + n(5) + "000" + n(5) + "1100011", Some(ImmType.B))
+  val BNE  = RVInst("BNE",  "b" + n(7) + n(5) + n(5) + "001" + n(5) + "1100011", Some(ImmType.B))
+  val BLT  = RVInst("BLT",  "b" + n(7) + n(5) + n(5) + "100" + n(5) + "1100011", Some(ImmType.B))
+  val BGE  = RVInst("BGE",  "b" + n(7) + n(5) + n(5) + "101" + n(5) + "1100011", Some(ImmType.B))
+  val BLTU = RVInst("BLTU", "b" + n(7) + n(5) + n(5) + "110" + n(5) + "1100011", Some(ImmType.B))
+  val BGEU = RVInst("BGEU", "b" + n(7) + n(5) + n(5) + "111" + n(5) + "1100011", Some(ImmType.B))
 
-  // R-type: op=0110011
-  val ADD  = RVInst("ADD",  "b0000000" + n(5) + n(5) + "000" + n(5) + "0110011", "R")
-  val SUB  = RVInst("SUB",  "b0100000" + n(5) + n(5) + "000" + n(5) + "0110011", "R")
-  val SLL  = RVInst("SLL",  "b0000000" + n(5) + n(5) + "001" + n(5) + "0110011", "R")
-  val SLT  = RVInst("SLT",  "b0000000" + n(5) + n(5) + "010" + n(5) + "0110011", "R")
-  val SLTU = RVInst("SLTU", "b0000000" + n(5) + n(5) + "011" + n(5) + "0110011", "R")
-  val XOR  = RVInst("XOR",  "b0000000" + n(5) + n(5) + "100" + n(5) + "0110011", "R")
-  val SRL  = RVInst("SRL",  "b0000000" + n(5) + n(5) + "101" + n(5) + "0110011", "R")
-  val SRA  = RVInst("SRA",  "b0100000" + n(5) + n(5) + "101" + n(5) + "0110011", "R")
-  val OR   = RVInst("OR",   "b0000000" + n(5) + n(5) + "110" + n(5) + "0110011", "R")
-  val AND  = RVInst("AND",  "b0000000" + n(5) + n(5) + "111" + n(5) + "0110011", "R")
+  // R-type: no imm, dontCare for QMC
+  val ADD  = RVInst("ADD",  "b0000000" + n(5) + n(5) + "000" + n(5) + "0110011", None)
+  val SUB  = RVInst("SUB",  "b0100000" + n(5) + n(5) + "000" + n(5) + "0110011", None)
+  val SLL  = RVInst("SLL",  "b0000000" + n(5) + n(5) + "001" + n(5) + "0110011", None)
+  val SLT  = RVInst("SLT",  "b0000000" + n(5) + n(5) + "010" + n(5) + "0110011", None)
+  val SLTU = RVInst("SLTU", "b0000000" + n(5) + n(5) + "011" + n(5) + "0110011", None)
+  val XOR  = RVInst("XOR",  "b0000000" + n(5) + n(5) + "100" + n(5) + "0110011", None)
+  val SRL  = RVInst("SRL",  "b0000000" + n(5) + n(5) + "101" + n(5) + "0110011", None)
+  val SRA  = RVInst("SRA",  "b0100000" + n(5) + n(5) + "101" + n(5) + "0110011", None)
+  val OR   = RVInst("OR",   "b0000000" + n(5) + n(5) + "110" + n(5) + "0110011", None)
+  val AND  = RVInst("AND",  "b0000000" + n(5) + n(5) + "111" + n(5) + "0110011", None)
 
-  val FENCE = RVInst("FENCE", "b" + n(7) + n(5) + n(5) + "000" + n(5) + "0001111", "I")
+  val FENCE = RVInst("FENCE", "b" + n(7) + n(5) + n(5) + "000" + n(5) + "0001111", None)
 
   val all: Seq[RVInst] = Seq(
     LUI, AUIPC, JAL, JALR,
@@ -86,12 +96,6 @@ object RiscvInsts {
   )
 }
 
-// -------- ChiselEnums for decode output (maintainable) --------
-
-object ImmType extends chisel3.ChiselEnum {
-  val I, S, B, U, J = Value
-}
-
 // -------- DecodeField helpers --------
 
 trait DecodeAPI {
@@ -100,25 +104,17 @@ trait DecodeAPI {
 
 // -------- DecodeFields --------
 
-/** Outputs UInt(width) so DecodeBundle does not cast to ImmType (avoids W001); use ImmType.safe() when reading. */
+/** Outputs UInt(width) so decode never casts to ImmType (avoids W001); use ImmType.safe() when reading. */
 object ImmTypeField extends DecodeField[RVInst, UInt] with DecodeAPI {
   def name = "imm_type"
   def chiselType = UInt(ImmType.getWidth.W)
-  def genTable(inst: RVInst): BitPat = inst.instType match {
-    case "I" => bitPatFor(ImmType.I)
-    case "S" => bitPatFor(ImmType.S)
-    case "B" => bitPatFor(ImmType.B)
-    case "U" => bitPatFor(ImmType.U)
-    case "J" => bitPatFor(ImmType.J)
-    case _   => BitPat.dontCare(ImmType.getWidth)
-  }
+  def genTable(inst: RVInst): BitPat = inst.immType.fold(BitPat.dontCare(ImmType.getWidth))(bitPatFor)
 }
 
 // -------- DecodedInst bundle & IDU module --------
 
 class DecodedInst extends Bundle {
-  val inst     = UInt(32.W)
-  val immType  = ImmType()
+  val imm     = UInt(32.W)
 }
 
 class IDU extends Module {
@@ -135,8 +131,16 @@ class IDU extends Module {
   val decodedRaw = decoder(QMCMinimizer, io.inst.bits, table)
   val (immType, _) = ImmType.safe(decodedRaw)
 
+  val inst = io.inst.bits
+  val immI = Cat(Fill(20, inst(31)), inst(31, 20))
+  val immS = Cat(Fill(20, inst(31)), inst(31, 25), inst(11, 7))
+  val immB = Cat(Fill(19, inst(31)), inst(31), inst(7), inst(30, 25), inst(11, 8), 0.U(1.W))
+  val immU = Cat(inst(31, 12), 0.U(12.W))
+  val immJ = Cat(Fill(11, inst(31)), inst(31), inst(19, 12), inst(20), inst(30, 21), 0.U(1.W))
+
+  val imm = Mux1H(immType.asUInt, Seq(immI, immS, immB, immU, immJ))
+
   io.decoded.valid := io.inst.valid
-  io.decoded.bits.inst     := io.inst.bits
-  io.decoded.bits.immType  := immType
+  io.decoded.bits.imm     := imm
   io.inst.ready := io.decoded.ready
 }
